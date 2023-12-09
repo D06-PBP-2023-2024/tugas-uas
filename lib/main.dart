eec363ad63d6986d3379ec9cdf8168079becdb78
rigifdaga <rfdauliaa@gmail.com>
2023-12-09 17:54:12 +0700

    integrasi login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tugas_uas/screen/home.dart';
-import 'login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:tugas_uas/screen/login.dart';
+
 
 
 void main() {
@@ -8,15 +10,21 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kindle Kids',
-      theme: ThemeData(
-        useMaterial3: true,
+    return Provider(
+      create: (_){
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Kindle Kids',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+      home: LoginPage(),
       ),
-      home: const Home(),
     );
   }
 }
