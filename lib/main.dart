4b0f486b0523e8ef9c6e6316ad48afcf16f7e477
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2023-12-12 21:13:46 +0700

    temporal loom overloading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tugas_uas/screen/home.dart';
+import 'login.dart';
 
 void main() {
   runApp(const App());
@@ -12,56 +12,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Kindle Kids',
       theme: ThemeData(
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const Home(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: LoginPage(), // Change this line
     );
   }
 }
