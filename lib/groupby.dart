665bbe940ba145350f2698668850c843d1cc73b8
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2023-12-18 15:49:13 +0700

    Added details when clicked

--- lib/groupby.dart
+++ lib/groupby.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:tugas_uas/screen/book_detail.dart';
 
 class GroupBy extends StatefulWidget {
   const GroupBy({super.key});
@@ -91,8 +92,17 @@ class _GroupByState extends State<GroupBy> {
         title: Text(bookFields['title']),
         onTap: () {
           // Handle book tap
+
           print('Book tapped: ${bookFields['title']}');
           print(bookFields['cover_url']);
+
+          Navigator.push(context, MaterialPageRoute(
+                          builder: (context) {
+                            return BookDetail(
+                              id: bookInfo['id'],
+                            );
+                          },
+                        ));
         },
       );
     }).toList();
