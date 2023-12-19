ff466ad812682af5d08350e4fa25e5c53722c1b2
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2023-12-19 22:46:38 +0700

    dsdsd

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:tugas_uas/filter.dart';
 import 'package:tugas_uas/screen/login.dart';
 import 'package:tugas_uas/screen/profile.dart';
 import 'package:tugas_uas/screen/home.dart';
