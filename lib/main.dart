6b41ed47d5ccb1e7541fd046eff52e1a4f77245c
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2023-12-19 22:35:00 +0700

    merge rachel

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:tugas_uas/screen/book_detail.dart';
+import 'package:tugas_uas/screen/login.dart';
 
 void main() {
   runApp(const App());
